99362c71b51004458a623e2562a08b2c762073e7
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-10-25 21:53:03 +0200

    homepage

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,6 @@
 class ActivitiesController < ApplicationController
+
+    def index
+        @activities = Activity.all
+    end
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,2 +1,7 @@
 class GuestsController < ApplicationController
+
+    def index
+        @guests = Guest.all
+    end
+
 end
