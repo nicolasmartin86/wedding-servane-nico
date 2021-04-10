64563a61e9c4b1389cb02e769145ca623ccfcf8f
Nicolas Martin <nicolas.artur.martin@gmail.com>
2021-04-10 11:58:00 +0200

    enable multiple emails

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class Guest < ApplicationRecord
     validates :first_name, :last_name, :address, presence: true
-    validates :email, uniqueness: true, format: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+    validates :email, uniqueness: false, format: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
 
 
     def self.to_csv(options)
